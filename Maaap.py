4ec0d02473197ec3666e9e4d8aba1ea6e7fe3823
cadencranford12133 <caden.cranford@ptaaschool.net>
2022-04-25 08:39:31 -0500

    kctcjhvbd,xb d j

---
+++ Maaap.py
@@ -0,0 +1,23 @@
+from curses import window
+from distutils.command.config import config
+import tkinter as tk
+from turtle import onclick
+from PIL import Image, ImageTK
+from urllib.request import urlopen
+from io import BytesIO
+import json
+
+from setuptools import setup
+import config as cfg
+
+def refresh(x):
+    pass
+
+#Window setup
+root= tk.TK()
+root.title("map program - International space station Location")
+
+display_map = tk.Label(root,font=('Arial 18 bold'),fg='red')
+display_map.bind('<Buttion',refresh)
+
+root.mainloop()
